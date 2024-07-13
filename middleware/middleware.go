892d4818d688ccb70988cbafb8bc25e81e6416d6
davidhorvat <david.horvat@gauss.dev>
2024-07-13 14:16:19 +0200

    chore: stuff

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -117,7 +117,7 @@ func (m *Middleware) User() gin.HandlerFunc {
 
 func (m *Middleware) CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://password-lock-frontend:5173")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
