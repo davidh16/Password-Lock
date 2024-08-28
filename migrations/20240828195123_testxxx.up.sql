4b3808d4ce606fee0be7302a7b49d48b314cae9e
davidhorvat <david.horvat@gauss.dev>
2024-08-28 21:11:05 +0200

    chore: remove test migration

--- migrations/20240828195123_testxxx.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS testxxx (
-	uuid uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-	/*----------------------------*/
-    -- extras
-
-);
