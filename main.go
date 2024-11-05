49d6f16a85728d2f24f2d91a4804f16bbf904bd7
davidhorvat <david.horvat@gauss.dev>
2024-11-05 20:53:23 +0100

    chore: add log

--- main.go
+++ main.go
@@ -3,40 +3,36 @@ package main
 import (
 	"fmt"
 	"password-lock/config"
-	"password-lock/controller"
-	"password-lock/db"
-	mw "password-lock/middleware"
-	"password-lock/repository"
-	"password-lock/server"
-	"password-lock/service"
 )
 
 func main() {
 
 	cfg := config.GetConfig()
 
-	redis := db.ConnectToRedis(cfg)
+	fmt.Println("environment : ", cfg.Environment)
 
-	pgInstance := db.ConnectToDatabase(cfg)
-
-	userRepo := repository.NewUserRepository(pgInstance)
-	entityRepo := repository.NewEntityRepository(pgInstance)
-	tokenRepo := repository.NewTokenRepository(pgInstance)
-
-	svc := service.NewService(
-		redis,
-		cfg,
-		userRepo,
-		entityRepo,
-		tokenRepo,
-	)
-
-	ctrl := controller.NewController(svc)
-
-	middleware := mw.InitializeMiddleware(pgInstance, redis, cfg)
-
-	srv := server.NewServer(ctrl, middleware, cfg)
-
-	// Listen and Server in 0.0.0.0:8080
-	srv.Run(fmt.Sprintf("0.0.0.0:%s", cfg.Port))
+	//redis := db.ConnectToRedis(cfg)
+	//
+	//pgInstance := db.ConnectToDatabase(cfg)
+	//
+	//userRepo := repository.NewUserRepository(pgInstance)
+	//entityRepo := repository.NewEntityRepository(pgInstance)
+	//tokenRepo := repository.NewTokenRepository(pgInstance)
+	//
+	//svc := service.NewService(
+	//	redis,
+	//	cfg,
+	//	userRepo,
+	//	entityRepo,
+	//	tokenRepo,
+	//)
+	//
+	//ctrl := controller.NewController(svc)
+	//
+	//middleware := mw.InitializeMiddleware(pgInstance, redis, cfg)
+	//
+	//srv := server.NewServer(ctrl, middleware, cfg)
+	//
+	//// Listen and Server in 0.0.0.0:8080
+	//srv.Run(fmt.Sprintf("0.0.0.0:%s", cfg.Port))
 }
